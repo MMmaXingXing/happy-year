6db770d5afe5d0a594a02c670bfdf5ad7c7a9ac7
Hong <1172406997@qq.com>
2022-01-28 19:17:41 +0800

    feat: 图片资源优化

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import door from "../images/door.png"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+// css
+import "./index.css"
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <img src={door} className="door" />
     {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
+      src="../images/door.png"
+      width={"980px"}
       quality={95}
       formats={["auto", "webp", "avif"]}
       alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
     /> */}
   </Layout>
 )
