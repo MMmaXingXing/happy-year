36ce9877832e4290379cc2eb1cbdd873e77d5457
Hong <1172406997@qq.com>
2022-01-29 17:21:14 +0800

    feat: 打包文件路径更新

---
+++ README.md
@@ -0,0 +1,19 @@
+# Happy New Year
+
+无论如何，新年快乐鸭！～～～
+
+## 项目运行
+
+```javascript
+    yarn install
+
+    yarn develop
+```
+
+## 项目打包
+
+```javascript
+    yarn build // 不带参数打包
+
+    gatsby build --prefix-paths // 带参数配置
+```
--- code/gatsby-config.js
+++ code/gatsby-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // 打包静态项目配置
+  pathPrefix: `/happy-year`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
